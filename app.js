a7ad7c4fa55f02c95f49369f57a50d7befc8adbb
Sadhana <Learningdevops98@gmail.com>
2024-10-12 21:42:32 +0530

    exposed port 8080 in  app.js file

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Serve static files (like HTML, CSS, and client-side JS)
+app.use(express.static('public'));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
